d64a28135590eddadfc3349c576eae0e818fc31b
Mary Pavlovets <mashunya-1604@yandex.by>
2019-04-28 17:47:51 +0300

    preparing benchmarks

--- models.py
+++ models.py
@@ -1,5 +1,7 @@
 import subprocess
-
+from statsmodels.tsa.vector_ar.var_model import VAR
+import numpy as np
+from sklearn.linear_model import LinearRegression
 
 class TRMF:
     def __init__(self,
@@ -51,3 +53,65 @@ class TRMF:
     @staticmethod
     def compile_sources():
         subprocess.call('make -f Makefile'.split())
+
+class IndependentFeaturesAutoRegressionModel():
+    """ Forecasting with n independent AR models for timeseries with lags """
+    def __init__(self, lags):
+        self.lags = lags
+
+    def fit(self, train):
+        self.train = train
+        self.coefs = []
+        self.intercept = []
+
+        for index in range(len(train)):
+            series = train[index]
+            features = [np.roll(series, shift=lag+1) for lag in range(max(self.lags))]
+            
+            X = np.vstack([features])
+            model = LinearRegression()
+            model.fit(X[:,max(self.lags):].T, series[max(self.lags):])
+            self.intercept.append(model.intercept_)
+            self.coefs.append(model.coef_)
+
+    def predict(self, h):
+        preds = []
+        for index in range(len(self.train)):
+            series = self.train[index]
+            for _ in range(h):
+                features = np.array([series[-(lag+1)] for lag in range(max(self.lags))])
+                pred = (self.coefs[index] * features).sum() + self.intercept[index]
+                series = np.append(series, pred)
+            preds.append(series[-h:])
+        return np.vstack(preds)
+
+class AutoRegressionModel():
+    """ Forecasting with AR model for each timeseries with lags """
+    def __init__(self, lags):
+        self.lags = lags
+
+    def fit(self, train):
+        self.train = train.T
+        self.model = VAR(endog=self.train)
+        self.model_fit = self.model.fit(maxlags=max(self.lags), trend='nc')
+
+    def predict(self, h):
+        res = self.model_fit.forecast(self.model_fit.y[-self.model_fit.k_ar:,:], steps=h)
+        return res.T
+
+class SvdAutoRegressionModel():
+    """ Forecasting with SVD AR model for each timeseries with lags """
+    def __init__(self, lags):
+        self.lags = lags
+    
+    def fit(self, train):
+        u, s, vh = np.linalg.svd(train, full_matrices=False)
+        self.F = u*s
+        self.V = vh
+        self.model = VAR(endog=self.V)
+        self.model_fit = self.model.fit(maxlags=max(self.lags), trend='nc')
+
+    def predict(self, h):
+        res = self.model_fit.forecast(self.model_fit.y[-self.model_fit.k_ar:,:], steps=h)
+        res = np.dot(self.F,res.T)
+        return res
