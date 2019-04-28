7e8b58ed7a36f2f527c1bd3119dccca208e30f1b
mihail <misha.kukuev@gmail.com>
2019-04-28 10:47:27 +0300

    Makefile added

---
+++ Makefile
@@ -0,0 +1,2 @@
+all:
+	g++ -I Eigen -std=c++11 -O3 trmf.cpp -o trmf
--- run_example.sh
+++ run_example.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 ./aggregate.py --data_dir history --begin_date 01.06.2017 --end_date 01.11.2017 --output_filename agg
 echo "Data is aggregated! Compiling ..."
-g++ -I Eigen/ -std=c++11 -O3 trmf.cpp -o trmf
+make -f Makefile
 echo "Compiled! Starting TRMF ..."
 ./trmf --input_file agg_values.csv --output_file result.txt
