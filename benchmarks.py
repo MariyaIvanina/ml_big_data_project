f1d8f57aee1acd95536d149d4088a3321e3dce59
Mary Pavlovets <mashunya-1604@yandex.by>
2019-04-28 22:30:30 +0300

    recaclualted plots and benchmarks

--- benchmarks.py
+++ benchmarks.py
@@ -20,22 +20,13 @@ def _parse_args():
     parser.add_argument('--end_date', help="End date for training in format 'dd.mm.yyyy'")
     parser.add_argument('--columns', default='O,C,H,L', help="List of columns to fetch, separated by ','")
 
-    # TRMF params
-    # parser.add_argument('--rank', default=32, help="Factorization rank")
-    # parser.add_argument('--lambda_x', default=1000, help="Regularization coefficient for inconsistent with"
-    #                                                      " autoregression model rows in X matrix")
-    # parser.add_argument('--lambda_w', default=1000, help="Regularization coefficient for large autoregression"
-    #                                                      " coefficients in W matrix")
-    # parser.add_argument('--lambda_f', default=0.01, help="Regularization coefficient for large values in F matrix")
-    # parser.add_argument('--eta', default=0.001, help="Regularization coefficient eta for X matrix")
-
     return parser.parse_args()
 
 
 def apply_benchmark_model(data_interpolated, model_name):
     T = data_interpolated.shape[1]
     errors_ind_ar = []
-    for lags in [[1], [1, 7], [1, 7, 14], [1, 7, 14, 30], [1, 2, 3, 4, 5, 6, 7, 14, 30]]:
+    for lags in [[1, 2, 3, 4, 5, 6, 7, 14, 21]]:
         temp = []
         for h in [1, 5, 10, 20]:
             model = model_name(lags=lags)
